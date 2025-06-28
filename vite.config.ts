61640f51bd608288beff112aaf2d635665073052
liuzheng <332644263@qq.com>
2025-06-28 21:28:07 +0800

    fix: 将CSS内联到JS中，解决用户需要单独引入CSS文件的问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cssInjectedByJsPlugin()],
   build: {
     lib: {
       entry: "src/index.ts",
       name: "ReactUploader",
       fileName: (format) => `react-uploader.${format}.js`,
     },
+    cssCodeSplit: false,
     rollupOptions: {
       external: ["react", "react-dom"],
       output: {
@@ -16,7 +18,18 @@ export default defineConfig({
           react: "React",
           "react-dom": "ReactDOM",
         },
+        inlineDynamicImports: false,
+        manualChunks: undefined,
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith(".css")) {
+            return "style.[hash].css";
+          }
+          return "[name].[hash][extname]";
+        },
       },
     },
+  },
+  css: {
+    modules: false,
   },
 });
