f0f9a53e78d3033436062b92f0be3b9fe9e9b873
liuzheng <332644263@qq.com>
2025-06-28 19:55:52 +0800

    修复TypeScript类型定义文件生成问题，移除Demo组件导出

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export { Uploader } from "./components/Uploader/Uploader";
 export { DialogUploader } from "./components/DialogUploader/DialogUploader";
 export { UploadButton } from "./components/UploadButton/UploadButton";
-export { Demo } from "./stories/Demo";
 
 export { useUploadQueue, useFileSelection, useUploadUI } from "./hooks";
 export type { UploadProgress, UploadOptions, UploadResult } from "./hooks";
